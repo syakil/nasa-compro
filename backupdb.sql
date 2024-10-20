a737853d55ceab36538c30b93893bfa0efe0f97d
Syakil Ahmad Ginanjar <46291036+syakil@users.noreply.github.com>
2024-10-21 00:13:48 +0700

    Update backupdb.sql

--- backupdb.sql
+++ backupdb.sql
@@ -16,7 +16,7 @@
 
 
 -- Dumping database structure for nusa
-DROP DATABASE IF EXISTS `nusa`;
+DROP DATABASE IF EXISTS `nasa`;
 CREATE DATABASE IF NOT EXISTS `nusa` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `nusa`;
 
